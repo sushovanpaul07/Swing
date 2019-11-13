865992917f6f17aba9948263495bdeae911e2ddf
sushovanpaul07 <49668915+sushovanpaul07@users.noreply.github.com>
2019-11-13 21:55:53 +0530

    Create apples.java

---
+++ apples.java
@@ -0,0 +1,12 @@
+
+import javax.swing.JFrame;
+
+class apples{
+    public static void main(String args[])
+    {
+        tuna bucky= new tuna();
+        bucky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        bucky.setSize(200,300);
+        bucky.setVisible(true);
+    }
+}
